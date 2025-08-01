9e71d99b05939b098d4912be448f7d3ac1af4007
Ilya <shcherbakov.illya@list.ru>
2025-08-02 03:10:21 +0400

    fix bullets

--- js/main.js
+++ js/main.js
@@ -1,59 +1,59 @@
 const tabs = document.querySelectorAll('.header__tab');
 const cards = document.querySelectorAll('.cards__item');
 
+const getVisibleSlides = () => {
+    // Получаем все слайды и фильтруем только видимые
+    return Array.from(document.querySelectorAll('.swiper-slide')).filter(slide => !slide.classList.contains('hidden'));
+};
+
+const renderBullet = (index, className) => {
+    const visibleSlides = getVisibleSlides();
+    // Если индекс меньше количества видимых слайдов, создаем пагинацию
+    return index < visibleSlides.length ? `<span class="${className}"></span>` : '';
+};
+
+const swiper = new Swiper('.cards__slider', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: renderBullet
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 25
+        },
+        481: {
+            slidesPerView: 2,
+            spaceBetween: 25
+        },
+        801: {
+            slidesPerView: 6
+        }
+    }
+});
+
 tabs.forEach(tab => {
     tab.addEventListener('click', function() {
         tabs.forEach(tab => tab.classList.remove('header__tab-active'));
         tab.classList.add('header__tab-active');
         const filter = tab.dataset.filter;
+
         cards.forEach(card => {
             if (filter === 'all' || card.dataset.category === filter) {
                 card.classList.remove('hidden');
-                setTimeout(()=>{
-                    // С помощью setTimeout делаем отображение более плавным
+                setTimeout(() => {
                     card.classList.remove('cards__item-opacity');
-                }, 100)
+                }, 100);
             } else {
                 card.classList.add('hidden');
-                setTimeout(()=>{
+                setTimeout(() => {
                     card.classList.add('cards__item-opacity');
-                }, 100)
+                }, 100);
             }
         });
+
+        // Обновляем Swiper после изменения видимости карточек
+        swiper.update();
     });
 });
-
-new Swiper('.cards__slider', {
-    // Optional parameters
-    pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-            },
-    //slidesPerView:6,
-    breakpoints:{
-        
-        320: {
-            slidesPerView: 1,
-            spaceBetween: 25,
-            pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-            }
-        },
-        481: {
-            slidesPerView:2,
-            spaceBetween:25,
-            pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-            }
-        },
-        801:{
-            slidesPerView:6
-        }
-    }
-
-    // If we need pagination
-    
-})
-  
